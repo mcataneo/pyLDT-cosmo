5c0e42e1f67a802e749a7439db78022baf54be21
Matteo Cataneo <mcataneo85@gmail.com>
2021-01-14 12:11:59 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyLDT", # Replace with your own username
+    version="0.0.1",
+    author="Matteo Cataneo",
+    author_email="mcataneo85@gmail.com",
+    description="Package for PDF calculations in Large Deviation Theory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcataneo/pyLDT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
