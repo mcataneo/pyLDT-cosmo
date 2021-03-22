056c0c4fe1bac212851c7237c45507f209a1f68e
Matteo Cataneo <mcataneo85@gmail.com>
2021-03-22 17:14:32 +0000

    updated to work with class object and conda environment

--- pyLDT_cosmo/tests/test_pyLDT.py
+++ pyLDT_cosmo/tests/test_pyLDT.py
@@ -2,6 +2,14 @@ import numpy as np
 import pyLDT_cosmo.pyLDT as pyLDT
 from pkg_resources import resource_stream
 
+import sys, os 
+is_conda = os.path.exists(os.path.join(sys.prefix, 'conda-meta'))
+
+# if running pyLDT from conda environment disable compilation to avoid conflicts
+if is_conda:
+    from julia.api import Julia
+    jl = Julia(compiled_modules=False)
+
 def test_pdf():
     #load benchmarks
     rho = np.loadtxt(resource_stream('pyLDT_cosmo', 'benchmarks/rho.dat'))
@@ -13,12 +21,9 @@ def test_pdf():
     R = np.array([10.]) # radius of top-hat smoothing filter in Mpc/h
     cosmo_params_fid = {'Omega_m': 0.31315, 'Omega_b': 0.0492, 'A_s': 2.0968e-9, 'n_s': 0.9652, 'h': 67.37}
     s2_mu_gr_fid = np.array([[0.391930, 0.252758, 0.166748]])
-    sig2_gr_fid = pyLDT.init_fid(cosmo_params_fid, z)
-
-    # Set cosmological parameters
-    cosmo_params = cosmo_params_fid
-    # Compute matter PDF for requested models
-    pdf_mat = pyLDT.compute_pdf(cosmo_params, z, R, sig2_gr_fid, s2_mu_gr_fid)
+    mPDF = pyLDT.matter_pdf(cosmo_params_fid, z, R, s2_mu_gr_fid)
+    # Compute matter PDF for fiducial cosmology
+    pdf_mat = mPDF.compute_pdf(cosmo_params_fid)
 
     assert np.allclose(pdf_mat['gr'][0][0](rho), pdf_z0)
     assert np.allclose(pdf_mat['gr'][0][1](rho), pdf_z0p5)
