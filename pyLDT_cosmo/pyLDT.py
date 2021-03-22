a722426d42c62323edd5eca21bce8d53b677ca62
Matteo Cataneo <mcataneo85@gmail.com>
2021-03-22 15:08:33 +0000

    completed creation of matter_pdf class and updated notebook accordingly

--- pyLDT_cosmo/pyLDT.py
+++ pyLDT_cosmo/pyLDT.py
@@ -27,8 +27,32 @@ init_ode()
 
 class matter_pdf:
 
-    def __init__(self):
-        pass
+    def __init__(self, params, zvec, Rvec, s2_mu_fid):
+
+        self.z = np.asarray(zvec, dtype=np.float64)
+        self.R = np.asarray(Rvec, dtype=np.float64)
+
+        Nz = len(zvec)
+
+        # To get P(k) at all redshifts rescale CAMB output at z=0 with LCDM growth (neglects perturbative and background effect from radiation)
+        # This approach is more self-consistent with MG rescaling below, but could be avoided if using MGCAMB/EFTCAMB/Hi_CLASS to get P(k) in MG instead
+        k_camb, _, pk_camb = compute_pk.compute_pk_gr(params,[0.])
+        D0_lambda = scipy.integrate.quadrature(growth_eqns.growth_int,1e-10,1.,args=(params['Omega_m'],1e-10,1.),tol=1e-8,rtol=1e-8)[0]
+        Dz_lambda_norm = np.array([scipy.integrate.quadrature(growth_eqns.growth_int,1e-10,1/(1+zf),args=(params['Omega_m'],1e-10,1/(1+zf)),tol=1e-8,rtol=1e-8)[0] for zf in zvec])/D0_lambda
+        Dz_reshaped = Dz_lambda_norm.reshape(len(Dz_lambda_norm),1)
+        pk_camb_z = np.repeat(Dz_reshaped**2, len(pk_camb[0]), axis=1) * np.repeat(pk_camb, Nz, axis=0)
+        logpk = [scipy.interpolate.CubicSpline(np.log(k_camb),np.log(pk_camb_z[i])) for i in range(Nz)]
+        #####################################
+
+        def pk(k,z_ix):
+            return np.exp(logpk[z_ix](np.log(k)))
+
+        k_sample = np.logspace(-4,np.log10(20),num=512)
+        self.sigma2_fid = self.compute_sigma2(k_sample,Nz,pk)
+
+        self.s2_mu_fid = s2_mu_fid
+        self.tau = self.get_tau(1., zf=0) # fix spherical evolution to EdS; sensitivity to DE/MG is negligible anyway
+
 
     def get_tau(self, Omega_m, zf=0):
 
@@ -68,26 +92,26 @@ class matter_pdf:
         return sigma2
 
 
-    def get_s2_mu(self, R, sigma2, sigma2_fid, s2_mu_fid):
+    def get_s2_mu(self, sigma2):
 
-        s2_mu = np.zeros_like(s2_mu_fid)
+        s2_mu = np.zeros_like(self.s2_mu_fid)
 
-        for ix,rth in enumerate(R):
+        for ix,rth in enumerate(self.R):
             sigma2_all_z = np.asarray([sig2(rth) for sig2 in sigma2])
-            sigma2_fid_all_z = np.asarray([sig2(rth) for sig2 in sigma2_fid])
-            s2_mu[ix] = np.log(1 + sigma2_all_z)/np.log(1 + sigma2_fid_all_z) * s2_mu_fid[ix]
-
+            sigma2_fid_all_z = np.asarray([sig2(rth) for sig2 in self.sigma2_fid])
+            s2_mu[ix] = np.log(1 + sigma2_all_z)/np.log(1 + sigma2_fid_all_z) * self.s2_mu_fid[ix]
+        
         return s2_mu
+    
 
+    def sigma2_gr(self, params):
 
-    def sigma2_gr(self, params, zvec):
-
-        Nz = len(zvec)
+        Nz = len(self.z)
         # To get P(k) at all redshifts rescale CAMB output at z=0 with LCDM growth (neglects perturbative and background effect from radiation)
         # This approach is more self-consistent with MG rescaling below, but could be avoided if using MGCAMB/EFTCAMB/Hi_CLASS to get P(k) in MG instead
         k_camb, _, pk_camb = compute_pk.compute_pk_gr(params,[0.])
         D0_lambda = scipy.integrate.quadrature(growth_eqns.growth_int,1e-10,1.,args=(params['Omega_m'],1e-10,1.),tol=1e-8,rtol=1e-8)[0]
-        Dz_lambda_norm = np.array([scipy.integrate.quadrature(growth_eqns.growth_int,1e-10,1/(1+zf),args=(params['Omega_m'],1e-10,1/(1+zf)),tol=1e-8,rtol=1e-8)[0] for zf in zvec])/D0_lambda
+        Dz_lambda_norm = np.array([scipy.integrate.quadrature(growth_eqns.growth_int,1e-10,1/(1+zf),args=(params['Omega_m'],1e-10,1/(1+zf)),tol=1e-8,rtol=1e-8)[0] for zf in self.z])/D0_lambda
         Dz_reshaped = Dz_lambda_norm.reshape(len(Dz_lambda_norm),1)
         pk_camb_z = np.repeat(Dz_reshaped**2, len(pk_camb[0]), axis=1) * np.repeat(pk_camb, Nz, axis=0)
         logpk = [scipy.interpolate.CubicSpline(np.log(k_camb),np.log(pk_camb_z[i])) for i in range(Nz)]
@@ -101,11 +125,11 @@ class matter_pdf:
         return sigma2, k_camb, pk_camb
 
 
-    def sigma2_fr(self, params, zvec, kvec, pk_gr):
+    def sigma2_fr(self, params, kvec, pk_gr):
 
-        Nz = len(zvec)
+        Nz = len(self.z)
 
-        pk_vec = compute_pk.compute_pk_fr_vectorized(params['Omega_m'], params['fR0'], params['n'], zvec, kvec, pk_gr)
+        pk_vec = compute_pk.compute_pk_fr_vectorized(params['Omega_m'], params['fR0'], params['n'], self.z, kvec, pk_gr)
         logpk = [scipy.interpolate.CubicSpline(np.log(kvec),np.log(pk_vec[i])) for i in range(Nz)]
 
         def pk(k,z_ix):
@@ -117,11 +141,11 @@ class matter_pdf:
         return sigma2
 
 
-    def sigma2_dgp(self, params, zvec, kvec, pk_gr):
+    def sigma2_dgp(self, params, kvec, pk_gr):
 
-        Nz = len(zvec)
+        Nz = len(self.z)
 
-        pk_vec = compute_pk.compute_pk_dgp_vectorized(params['Omega_m'], params['rcH0'], zvec, pk_gr)
+        pk_vec = compute_pk.compute_pk_dgp_vectorized(params['Omega_m'], params['rcH0'], self.z, pk_gr)
         logpk = [scipy.interpolate.CubicSpline(np.log(kvec),np.log(pk_vec[i])) for i in range(Nz)]
 
         def pk(k,z_ix):
@@ -133,92 +157,70 @@ class matter_pdf:
         return sigma2
 
 
-    def init_fid(self, params, zvec):
+    def get_pdf(self, s2_mu, sigma2):
 
-        Nz = len(zvec)
-
-        # To get P(k) at all redshifts rescale CAMB output at z=0 with LCDM growth (neglects perturbative and background effect from radiation)
-        # This approach is more self-consistent with MG rescaling below, but could be avoided if using MGCAMB/EFTCAMB/Hi_CLASS to get P(k) in MG instead
-        k_camb, _, pk_camb = compute_pk.compute_pk_gr(params,[0.])
-        D0_lambda = scipy.integrate.quadrature(growth_eqns.growth_int,1e-10,1.,args=(params['Omega_m'],1e-10,1.),tol=1e-8,rtol=1e-8)[0]
-        Dz_lambda_norm = np.array([scipy.integrate.quadrature(growth_eqns.growth_int,1e-10,1/(1+zf),args=(params['Omega_m'],1e-10,1/(1+zf)),tol=1e-8,rtol=1e-8)[0] for zf in zvec])/D0_lambda
-        Dz_reshaped = Dz_lambda_norm.reshape(len(Dz_lambda_norm),1)
-        pk_camb_z = np.repeat(Dz_reshaped**2, len(pk_camb[0]), axis=1) * np.repeat(pk_camb, Nz, axis=0)
-        logpk = [scipy.interpolate.CubicSpline(np.log(k_camb),np.log(pk_camb_z[i])) for i in range(Nz)]
-        #####################################
-
-        def pk(k,z_ix):
-            return np.exp(logpk[z_ix](np.log(k)))
-
-        k_sample = np.logspace(-4,np.log10(20),num=512)
-        sigma2 = self.compute_sigma2(k_sample,Nz,pk)
-
-        return sigma2
-
-
-    def get_pdf(self, R, z, tau, s2_mu, sigma2):
+        z = self.z
+        R = self.R
+        tau = self.tau
+        Nz = len(z)
+        Nr = len(R)
 
         rho_vec = np.linspace(0.1,13.5,1000)
 
-        def get_psi(R, tau, s2_mu, sigma2):
+        def get_psi(R, s2_mu, sigma2):
         # Here R, s2_mu and sigma2 refer to particular elements in the corresponding input arrays
             psi_vec = (sigma2(R)/s2_mu) * (tau(rho_vec)**2/(2*sigma2(R*rho_vec**(1/3))))
             return scipy.interpolate.CubicSpline(rho_vec, psi_vec)
 
-        psi_mat = [[get_psi(rth, tau, s2_mu[r_ix,z_ix], sigma2[z_ix]) for z_ix in range(len(sigma2))] for r_ix,rth in enumerate(R)]
+        psi_mat = [[get_psi(rth, s2_mu[r_ix,z_ix], sigma2[z_ix]) for z_ix in range(Nz)] for r_ix,rth in enumerate(R)]
 
         def get_pdf_nn(psi):
         # compute non-normalised PDF
             pdf_nn_vec = np.sqrt((psi(rho_vec,2) + psi(rho_vec,1)/rho_vec)/(2*np.pi))*np.exp(-psi(rho_vec))
             return scipy.interpolate.CubicSpline(rho_vec, pdf_nn_vec)
 
-        pdf_nn_mat = [[get_pdf_nn(psi_mat[r_ix][z_ix]) for z_ix in range(len(sigma2))] for r_ix in range(len(R))]
+        pdf_nn_mat = [[get_pdf_nn(psi_mat[r_ix][z_ix]) for z_ix in range(Nz)] for r_ix in range(Nr)]
 
         def mean_igr(rho, pdf_nn):
         # non-normalised PDF mean integrand
             return rho * pdf_nn(rho)
 
-        mean_mat = [[scipy.integrate.quad(mean_igr,0.1,10,args=(pdf_nn_mat[r_ix][z_ix]),epsabs=1e-4,epsrel=1e-4,limit=50)[0] for z_ix in range(len(sigma2))] for r_ix in range(len(R))]
-        norm_mat = [[scipy.integrate.quad(pdf_nn_mat[r_ix][z_ix],0.1,10,epsabs=1e-4,epsrel=1e-4,limit=50)[0] for z_ix in range(len(sigma2))] for r_ix in range(len(R))]
+        mean_mat = [[scipy.integrate.quad(mean_igr,0.1,10,args=(pdf_nn_mat[r_ix][z_ix]),epsabs=1e-4,epsrel=1e-4,limit=50)[0] for z_ix in range(Nz)] for r_ix in range(Nr)]
+        norm_mat = [[scipy.integrate.quad(pdf_nn_mat[r_ix][z_ix],0.1,10,epsabs=1e-4,epsrel=1e-4,limit=50)[0] for z_ix in range(Nz)] for r_ix in range(Nr)]
 
         def pdf(pdf_nn,mean,norm):
             rho_max = 10.
             pdf_vec = pdf_nn(rho_vec[rho_vec<rho_max] * mean/norm) * mean/norm**2
             return scipy.interpolate.CubicSpline(rho_vec[rho_vec<rho_max], pdf_vec)
 
-        pdf_mat = [[pdf(pdf_nn_mat[r_ix][z_ix],mean_mat[r_ix][z_ix],norm_mat[r_ix][z_ix]) for z_ix in range(len(sigma2))] for r_ix in range(len(R))]
+        pdf_mat = [[pdf(pdf_nn_mat[r_ix][z_ix],mean_mat[r_ix][z_ix],norm_mat[r_ix][z_ix]) for z_ix in range(Nz)] for r_ix in range(Nr)]
 
         return pdf_mat
 
 
-    def compute_pdf(cosmo_params, z, R, sig2_gr_fid, s2_mu_gr_fid, want_fr=False, want_dgp=False):
+    def compute_pdf(self, cosmo_params, want_fr=False, want_dgp=False):
 
-    #Compute function tau(rho) at z=0. Neglecting z-dependence only introduces inaccuracies <0.3% up to z=2
-    tau = get_tau(cosmo_params['Omega_m'], zf=0)
-    # tau = get_tau(1., zf=0) # fix SC to EdS for testing purposes
+        # compute linear and non-linear variances
+        sig2_gr, k_camb, pk_camb = self.sigma2_gr(cosmo_params)
+        s2_mu_gr = self.get_s2_mu(sig2_gr)
+        if want_fr: 
+            sig2_fr = self.sigma2_fr(cosmo_params, k_camb, pk_camb)
+            s2_mu_fr = self.get_s2_mu(sig2_fr)
+        if want_dgp: 
+            sig2_dgp = self.sigma2_dgp(cosmo_params, k_camb, pk_camb)
+            s2_mu_dgp = self.get_s2_mu(sig2_dgp)
 
-    # compute linear and non-linear variances
-    sig2_gr, k_camb, pk_camb = sigma2_gr(cosmo_params, z)
-    s2_mu_gr = get_s2_mu(R, sig2_gr, sig2_gr_fid, s2_mu_gr_fid)
-    if want_fr: 
-        sig2_fr = sigma2_fr(cosmo_params, z, k_camb, pk_camb)
-        s2_mu_fr = get_s2_mu(R, sig2_fr, sig2_gr_fid, s2_mu_gr_fid)
-    if want_dgp: 
-        sig2_dgp = sigma2_dgp(cosmo_params, z, k_camb, pk_camb)
-        s2_mu_dgp = get_s2_mu(R, sig2_dgp, sig2_gr_fid, s2_mu_gr_fid)
+        # compute matter PDF
+        pdf_gr_mat = self.get_pdf(s2_mu_gr, sig2_gr)
+        if want_fr: 
+            pdf_fr_mat = self.get_pdf(s2_mu_fr, sig2_fr)
+        else:
+            pdf_fr_mat = None
+        if want_dgp: 
+            pdf_dgp_mat = self.get_pdf(s2_mu_dgp, sig2_dgp)
+        else:
+            pdf_dgp_mat = None
 
-    # compute matter PDF
-    pdf_gr_mat = get_pdf(R, z, tau, s2_mu_gr, sig2_gr)
-    if want_fr: 
-        pdf_fr_mat = get_pdf(R, z, tau, s2_mu_fr, sig2_fr)
-    else:
-        pdf_fr_mat = None
-    if want_dgp: 
-        pdf_dgp_mat = get_pdf(R, z, tau, s2_mu_dgp, sig2_dgp)
-    else:
-        pdf_dgp_mat = None
-
-    return {'gr':pdf_gr_mat, 'fr':pdf_fr_mat, 'dgp':pdf_dgp_mat}
+        return {'gr':pdf_gr_mat, 'fr':pdf_fr_mat, 'dgp':pdf_dgp_mat}
 
     ######################### Test functions below #########################
-
